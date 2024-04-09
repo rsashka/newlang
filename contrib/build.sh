cfd3aa7d4027511f75b6de80576adf1e375ac7ce
Aleksandr Ryabikov <rsashka@mail.ru>
2024-04-09 15:07:51 +0300

    FIX build contrib to download libthorch project

--- contrib/build.sh
+++ contrib/build.sh
@@ -4,7 +4,17 @@ cd text2cpp
 make
 make install
 
+echo Build Google UnitTest freamework ...
 cd ../googletest
 cmake .
 make
 cd ..
+
+echo Download libtorch ...
+if [ ! -e libtorch.zip ]; then
+  curl https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-2.2.2%2Bcpu.zip --output libtorch.zip
+fi
+
+echo Unzip libtorch ...
+unzip libtorch.zip -d .
+
